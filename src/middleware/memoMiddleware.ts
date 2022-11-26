a1c116c1e50698b4417e035388050dcdc77dd1f2
hoodie <popping2606@gmail.com>
2022-11-26 12:52:59 +0900

    세션 있는 경우만 체크

--- src/middleware/memoMiddleware.ts
+++ src/middleware/memoMiddleware.ts
@@ -4,25 +4,28 @@ import jwt from 'jsonwebtoken'
 
 export const sessionCheck = (req: Request, res: Response) => {
   const secret = process.env.GOOGLE_SECRET || ''
-  try {
-    const user = jwt.verify(
-      req.cookies.go_memo_session,
-      secret
-    ) as jwt.JwtPayload
+  // 세션 있는 경우 체크
+  if (req.cookies.go_memo_session) {
+    try {
+      const user = jwt.verify(
+        req.cookies.go_memo_session,
+        secret
+      ) as jwt.JwtPayload
 
-    if (user && user.email) {
-      req.body = {
-        ...req.body,
-        decodedUser: user,
+      if (user && user.email) {
+        req.body = {
+          ...req.body,
+          decodedUser: user,
+        }
+        console.log(
+          `### session: ${user.name}, ${user.email}, ${req.ip}, ${req.url}`
+        )
       }
-      console.log(
-        `### session: ${user.name}, ${user.email}, ${req.ip}, ${req.url}`
-      )
-    } else {
-      console.log(`### No session: ${req.ip}, ${req.url}`)
+    } catch (err) {
+      console.error(`sessionCheck error: ${req.ip}, ${req.url},
+  ${err}`)
     }
-  } catch (err) {
-    console.error(`sessionCheck error: ${req.ip}, ${req.url},
-${err}`)
+  } else {
+    console.log(`### No session: ${req.ip}, ${req.url}`)
   }
 }
