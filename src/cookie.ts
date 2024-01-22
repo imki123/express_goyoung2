690aeb4647c6e0dd2e931e1765e1ddedf0d2f84a
hoodie <popping2606@gmail.com>
2024-01-22 23:47:06 +0900

    feat: add partitioned option in cookie

--- src/cookie.ts
+++ src/cookie.ts
@@ -5,14 +5,19 @@ export const cookieKeys = {
   go_memo_session: 'go_memo_session',
 }
 
+declare module 'express' {
+  export interface CookieOptions {
+    partitioned?: boolean
+  }
+}
+
 const setCookieSecure = (): CookieOptions => {
-  if (process.env.NODE_ENV === 'production') {
-    return {
-      secure: true,
-      sameSite: 'none',
-    }
-  } else {
-    return {}
+  // 예전에는 secure 옵션이 https에서만 됐었는데... 이제 로컬에서도 잘된다?
+  return {
+    httpOnly: true,
+    secure: true,
+    sameSite: 'none',
+    partitioned: true,
   }
 }
 
